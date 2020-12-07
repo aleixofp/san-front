760e988c768c756a0d2bb2f78a5c7ccf5c0c1bc4
Felipe Aleixo <aleixofp@gmail.com>
2020-12-07 20:20:49 -0300

    call node server.js

--- server.js
+++ server.js
@@ -26,8 +26,3 @@ var allowCrossDomain = function(req, res, next) {
 };
 
 app.use(allowCrossDomain);
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(application_root, "public")));
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
